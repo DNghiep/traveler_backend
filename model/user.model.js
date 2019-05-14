c3a535377486310761b27419752819e29b10f171
nquangcuong96 <49380380+nquangcuong96@users.noreply.github.com>
2019-05-14 16:47:07 +0700

    Update user model

--- model/user.model.js
+++ model/user.model.js
@@ -4,14 +4,12 @@ const Schema = mongoose.Schema;
 
 const userModel = new Schema({
     _id: Schema.Types.ObjectId,
-    email: { type: String, unique: true},
+    user_name: String,
+    account: String,
     password: String,
-    person: new Schema({
-        full_name: String,
-        citizen_id: String,
-        address: String,
-        phone: String
-    })
+    email: String,
+    address: String,
+    phone: String
 })
 
 
