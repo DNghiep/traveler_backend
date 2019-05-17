55b138b8f37c9528047a1fe9e7953fc69debb3fa
hiep.lh <kris.nguyen@unicorn.vn>
2019-05-17 12:58:41 +0700

    fix ticket model and add api view history

--- model/ticket.model.js
+++ model/ticket.model.js
@@ -4,11 +4,15 @@ const Schema = mongoose.Schema;
 
 const ticketModel = new Schema({
     _id: Schema.Types.ObjectId,
-    onwer_id: Schema.Types.ObjectId,
+    owner_id: Schema.Types.ObjectId,
     trip_id: Schema.Types.ObjectId,
-    status: ['Paid'],
+    status: String,
     total_fee: Number,
-    creation_date: Date
+    creation_date: Date,
+    depart_time: Date,
+    arrival_time: Date,
+    from_str: String,
+    to_str: String
 })
 
 module.exports = ticketModel;
